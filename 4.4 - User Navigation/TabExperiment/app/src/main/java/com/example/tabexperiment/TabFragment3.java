99445f2f3de6f5ab6207fce9192a12a7e90bf0cf
Christian Bedwell <Christian.8edwell@gmail.com>
2019-06-25 11:54:43 -0400

    Added CodeLabs tutorial for user navigation.

---
+++ 4.4 - User Navigation/TabExperiment/app/src/main/java/com/example/tabexperiment/TabFragment3.java
@@ -0,0 +1,20 @@
+package com.example.tabexperiment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabFragment3 extends Fragment {
+
+    public TabFragment3() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment3, container, false);
+    }
+}
